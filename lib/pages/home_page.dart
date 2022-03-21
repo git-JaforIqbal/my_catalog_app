36f451800e83a8ee5144b3d151a1b357ddc8b00a
Md. Jafor Iqbal <40732377+git-JaforIqbal@users.noreply.github.com>
2022-03-21 22:42:01 +0600

    route, pages, etc

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,7 +11,7 @@ class HomePage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text("Tech Speech Plus"),
+        title: Text("Tech Speech Plus+"),
       ),
       body: Center(
         child: Container(
